5772e0cb6bb620838e79fb59e97fb80729b02c9f
babak alizadeh <alizadeh.babak@live.com>
2014-01-04 04:07:54 +0330

    delete some buffer files

--- upload/buffer/users_page.0a2a335d2caa0dc9b194b8170d986130.s_fa_IR.rtpl.php
+++
@@ -1,5 +0,0 @@
-<div id="users_page" class="users_page" >
-	<script language="javascript" type="text/javascript" src="../plugins/users/scripts/users_page.js"></script>
-	<div id="msg" class="users_page_msg" ></div>
-	 سلام!  babak <a onclick="users_logout()"> خروج </a>
-</div>
